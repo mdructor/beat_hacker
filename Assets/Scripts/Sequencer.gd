2a1e95509e20e5286d8e99b65e5cfd5bb5c97137
Stanford Chastain <fordchastain@yahoo.com>
2020-02-08 17:55:10 -0600

    Dynamically added buttons to sequenceer

--- Assets/Scripts/Sequencer.gd
+++ Assets/Scripts/Sequencer.gd
@@ -6,11 +6,29 @@ var _isPlaying = false
 var _timer = false
 
 var _sampleButtons = []
-
+var _sequenceButtons = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var sampleButton = preload("res://Assets/Scenes/SampleButton.tscn")
+	var sequenceButton = preload("res://Assets/Scenes/SequenceButton.tscn")
+	var y = 10.0
+	
+	for i in range(8):
+		var samp_instance = sampleButton.instance()
+		samp_instance.set_position(Vector2(10.0,y))
+		_sampleButtons.append(samp_instance)
+		add_child(_sampleButtons[i])
+		
+		_sequenceButtons.append([])
+		var x = 64.0
+		for j in range(8):
+			var seq_instance = sequenceButton.instance()
+			seq_instance.set_position(Vector2(x, y))
+			_sequenceButtons[i].append(seq_instance)
+			add_child(_sequenceButtons[i][j])
+			x += 54
+		y += 52# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
