fbc7c158492e51299f28d293dfc0297198051550
Mason Dructor <masondructor@gmail.com>
2020-02-08 17:02:58 -0600

    Add work to SampleButton

--- Assets/Scripts/SampleButton.gd
+++ Assets/Scripts/SampleButton.gd
@@ -6,17 +6,19 @@ var _samplePack = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func initialize(buttonText, sampleName, samplePack = ""):
 	_buttonText = buttonText
 	_sampleName = sampleName
 	_samplePack = samplePack
+	find_node("SampleName").text = _buttonText
+		
 
-
-
+func _on_SampleButton_pressed():
+	var player = find_parent("SamplePlayer")
+	if _samplePack != "":
+		player = player.find_child(_samplePack)
+	player = player.find_child(_sampleName)
+	player.play()
