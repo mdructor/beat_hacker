fd9167266273a5a9d918e63990d90f520d2eeb6b
Stanford Chastain <fordchastain@yahoo.com>
2020-02-08 19:42:05 -0600

    Added SequenceButton script and textures

---
+++ Assets/Scenes/SequenceButton.gd
@@ -0,0 +1,39 @@
+extends TextureButton
+
+var _blueSquareDown = preload("res://Assets/Images/blue_square_down.png")
+var _blueSquare = preload("res://Assets/Images/blue_square.png")
+var _darkBlueSquare = preload("res://Assets/Images/dark_blue_square.png")
+var _redSquare = preload("res://Assets/Images/red_square.png")
+
+var _isChecked = false
+var _isEnabled = true
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_SequenceButton_pressed():
+	if (_isChecked):
+		texture_normal = _blueSquare
+	else:
+		texture_normal = _blueSquareDown
+		
+	_isChecked = !_isChecked
+
+func set_state(var state):
+	_isEnabled = state
+	
+func _light_up():
+	if (_isChecked):
+		texture_normal = _redSquare
+	else:
+		texture_normal = _darkBlueSquare
