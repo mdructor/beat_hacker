7a1279fd026eb43ab67d7b77c92f6c4b2faf52d6
Stanford Chastain <fordchastain@yahoo.com>
2020-02-08 21:07:24 -0600

    Updated light up colors

--- Assets/Scripts/SequenceButton.gd
+++ Assets/Scripts/SequenceButton.gd
@@ -2,14 +2,14 @@ extends TextureButton
 
 var _blueSquareDown = preload("res://Assets/Images/blue_square_down.png")
 var _blueSquare = preload("res://Assets/Images/blue_square.png")
-var _darkBlueSquare = preload("res://Assets/Images/dark_blue_square.png")
-var _redSquare = preload("res://Assets/Images/red_square.png")
+var _purpleSquare = preload("res://Assets/Images/purple_square.png")
+var _pinkSquare = preload("res://Assets/Images/pink_square.png")
 
 var _isChecked = false
 var _isEnabled = true
 
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 func _on_SequenceButton_pressed():
 	if (_isChecked):
@@ -25,9 +25,9 @@ func set_state(state):
 func _light_up(flag):
 	if (flag):
 		if (_isChecked):
-			texture_normal = _redSquare
+			texture_normal = _pinkSquare
 		else:
-			texture_normal = _darkBlueSquare
+			texture_normal = _purpleSquare
 	else:
 		if (_isChecked):
 			texture_normal = _blueSquareDown
