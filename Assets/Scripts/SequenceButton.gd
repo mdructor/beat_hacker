c2d4ce5887bbb31c7ae8cc0eda43f77a62995836
Stanford Chastain <fordchastain@yahoo.com>
2020-02-08 20:39:44 -0600

    Moved SequenceButton script

--- Assets/Scripts/SequenceButton.gd
+++ Assets/Scripts/SequenceButton.gd
@@ -7,19 +7,9 @@ var _redSquare = preload("res://Assets/Images/red_square.png")
 
 var _isChecked = false
 var _isEnabled = true
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_SequenceButton_pressed():
 	if (_isChecked):
